e0c2703c59115bae23de71b926332e4d7e615cbb
Lucca Pirovano <luccapirovano@gmx.com>
2025-09-28 04:50:13 -0400

    added the rest of the stuff from base44

--- nvidiaprogram.py
+++ nvidiaprogram.py
@@ -1,4 +1,24 @@
-#import tensorflow as tf
+import tensorflow as tf
 #print(tf.config.list_physical_devices('GPU'))
 import torch
 print(torch.cuda.is_available())
+pip install -q openai
+import openai
+openai.my_api_key = 'sk-...0LYA'
+import openai
+openai.api_key = 'sk-...0LYA'
+messages = [ {"role": "system", "content": 
+              "You are a intelligent assistant."} ]
+while True:
+    message = input("User: ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
+    
